d593d861a13115557b700b4603dccd361f3e06ca
Pablo Berganza <pberganza@applaudostudios.com>
2018-12-04 13:45:36 -0600

    :recycle: Change name inner to outer

--- avorango/types.py
+++ avorango/types.py
@@ -4,19 +4,19 @@ from .errors import InvalidFieldError
 class BaseType:
     _value = None
 
-    def getter(self, inner):
+    def getter(self, outer):
         return self._value
 
 
 class Integer(BaseType):
-    def setter(self, inner, value):
+    def setter(self, outer, value):
         if not isinstance(value, int):
             raise InvalidFieldError("Integer expected")
         self._value = value
 
 
 class String(BaseType):
-    def setter(self, inner, value):
+    def setter(self, outer, value):
         if not isinstance(value, str):
             raise InvalidFieldError("String expected")
         self._value = value
