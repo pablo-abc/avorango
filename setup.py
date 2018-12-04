5c398c9fb34c8e3bcab524aa0697ab2951adb6a5
Pablo Berganza <pberganza10@gmail.com>
2018-12-03 23:22:53 -0600

    :wrench: Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='avorango',
+    version='0.1.0',
+    author='Pablo Berganza',
+    author_email='pberganza10@gmail.com',
+    description='An ORM for ArangoDB',
+    packages=find_packages(),
+    install_requires=[
+        'python-arango',
+    ],
+)
