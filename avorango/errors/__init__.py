35f2458b797b915fef7023256116b8c63caa4a70
Pablo Berganza <pberganza10@gmail.com>
2018-12-05 01:55:42 -0600

    :sparkles: Add RequiredError

--- avorango/errors/__init__.py
+++ avorango/errors/__init__.py
@@ -1,10 +1,16 @@
-class InvalidFieldError(Exception):
+class BaseError(Exception):
     def __init__(self, message=None):
         if message is not None:
             self.message = message
 
 
-class DefinitionError(Exception):
-    def __init__(self, message=None):
-        if message is not None:
-            self.message = message
+class InvalidFieldError(BaseError):
+    message = "The field is invalid"
+
+
+class DefinitionError(BaseError):
+    message = "The defiition is invalid"
+
+
+class RequiredError(BaseError):
+    message = "The field is required"
