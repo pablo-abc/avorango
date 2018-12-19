675c55c3a32bb34df9726331340d294d9b525ac1
Pablo Berganza <pberganza10@gmail.com>
2018-12-18 21:56:54 -0600

    :sparkles: Add validation for vertex

--- avorango/edge/edge.py
+++ avorango/edge/edge.py
@@ -1,16 +1,15 @@
 from avorango.collection import Collection
+from avorango.errors import InvalidFieldError
 
 
 class Edge(Collection):
     _from_vertices = None
     _to_vertices = None
-    _to = None
-    _from = None
 
     def __init__(self,
-                 collectionname=None,
                  from_vertices=None,
-                 to_vertices=None):
+                 to_vertices=None,
+                 collectionname=None):
         self._collectionname = type(self).collection_name
         if self._graphname is not None:
             self._graph = self._session.graph(self._graphname)
@@ -22,9 +21,14 @@ class Edge(Collection):
         if collectionname is not None:
             self._collectionname = collectionname
         if from_vertices is not None:
-            self._from_vertices = from_vertices
+            if type(self)._from_vertices is None:
+                type(self)._from_vertices = []
+            type(self)._from_vertices = \
+                type(self)._from_vertices + from_vertices
         if to_vertices is not None:
-            self._to_vertices = to_vertices
+            if type(self)._to_vertices is None:
+                type(self)._to_vertices = []
+            type(self)._to_vertices = type(self)._to_vertices + to_vertices
 
     def __get__(self, obj, objtype):
         if obj is None:
@@ -32,7 +36,7 @@ class Edge(Collection):
         return obj.__dict__[self.name]
 
     def __set__(self, obj, val):
-        pass
+        obj.__dict__[self.name] = self._validate(val)
 
     def __set_name__(self, objtype, name):
         self._graph = objtype._graph
@@ -42,3 +46,17 @@ class Edge(Collection):
             self._collection = self._graph.edge_collection(name)
         else:
             self._collection = self._session.collection(name)
+        if type(self)._from_vertices is None:
+            type(self)._from_vertices = []
+        if objtype not in type(self)._from_vertices:
+            type(self)._from_vertices.append(objtype.collection_name)
+
+    def _validate(self, val):
+        for vertex in self._to_vertices:
+            if isinstance(val, list):
+                for v in val:
+                    self._validate(v)
+                return val
+            if type(val).collection_name == vertex:
+                return val
+        raise InvalidFieldError()
